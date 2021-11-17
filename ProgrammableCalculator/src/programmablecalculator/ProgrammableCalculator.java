4a77a17512b9417e3a1c184f086d3d1ec2f573f6
Lexander99 <alexander.desantis@gmail.com>
2021-11-17 10:07:16 +0100

    a try of push

--- ProgrammableCalculator/src/programmablecalculator/ProgrammableCalculator.java
+++ ProgrammableCalculator/src/programmablecalculator/ProgrammableCalculator.java
@@ -19,6 +19,8 @@ public class ProgrammableCalculator {
         System.out.println("hello world alfonso");
         System.out.println("Hello world2");
         System.out.println("Hello world3");
+        System.out.println("Hello I am Alexander");
+        
     }
     
 }
