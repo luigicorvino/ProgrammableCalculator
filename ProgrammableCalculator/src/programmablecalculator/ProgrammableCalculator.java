08cf91b4cbb120b1b63f5fb548c59627fb2a41a3
luigicorvino <luigicorvino1998@gmail.com>
2021-11-18 15:40:49 +0100

    Try Complex library

--- ProgrammableCalculator/src/programmablecalculator/ProgrammableCalculator.java
+++ ProgrammableCalculator/src/programmablecalculator/ProgrammableCalculator.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package programmablecalculator;
-
+import org.apache.commons.math3.complex.*;
 /**
  *
  * @author luigi
@@ -19,6 +19,13 @@ public class ProgrammableCalculator {
         System.out.println("hello world alfonso");
         System.out.println("Hello world2");
         System.out.println("Hello world3");
+        Complex c = new Complex(4,9);
+        ComplexFormat format=new ComplexFormat();
+        String s=format.format(c);
+        System.out.println(s);
+        Complex root=c.sqrt();
+        String s2=format.format(root);
+        System.out.println(s2);
     }
     
 }
